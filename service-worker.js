494ba8c16b0cd709797ec5529711273a7b8cc4dc
sjwickes9 <simon@cynapse.co.uk>
2026-07-01 20:08:29 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
- self.addEventListener('install', function(event) {
-  console.log("Service Worker installed");
-});
-
-self.addEventListener('fetch', function(event) {
-  // Basic offline support placeholder
-});
